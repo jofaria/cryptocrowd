008c705a274778e543858d42844a1c6aabd74ccd
Sillas Victor <Sillas_2@hotmail.com>
2021-11-23 15:53:45 +0000

    fix route path

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const isLoggedIn = require("./../middleware/isLoggedIn");
 const Event = require("./../models/event.model");
-const User = require("../models/user.model");
+const User = require("./../models/user.model");
 
 router.get("/profile", isLoggedIn, (req, res) => {
   res.render("user/profile");
